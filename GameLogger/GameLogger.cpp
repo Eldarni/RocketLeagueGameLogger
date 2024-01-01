7178a00c03cb68744956876fa394742c3064cf90
RickBaby <richard@smith-net.org.uk>
2024-01-01 12:05:25 +0000

    Add project files.

---
+++ GameLogger/GameLogger.cpp
@@ -0,0 +1,181 @@
+
+//
+#include "pch.h"
+
+//
+#include <ctime>
+#include <fstream>
+#include <sstream>
+
+//
+#include "GameLogger.h"
+
+//
+BAKKESMOD_PLUGIN(GameLogger, "Log game stats to CSV", plugin_version, PLUGINTYPE_FREEPLAY)
+
+//
+std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
+
+//@todo: get this from cvar
+std::string filename = "C:\\SCRAP\\GameLogger.csv";
+
+//prepare a map for the player stats
+std::map<std::string, PlayerAndMatchStats> playerStats;
+
+//Set up
+void GameLogger::onLoad() {
+
+
+    //
+	_globalCvarManager = cvarManager;
+
+    //update the player stats every kickoff - should deal with most issues around players leaving (won't be perfect but the only other option will be per tick)
+    gameWrapper->HookEvent("Function GameEvent_Soccar_TA.Active.StartRound", [this](std::string eventName) {
+        updatePlayerStats();
+    });
+
+	//main hook to write the stats to the csv - called at the end of the game - so if the player running the plugin rage quits
+	gameWrapper->HookEvent("Function TAGame.GameEvent_Soccar_TA.OnMatchEnded", std::bind(&GameLogger::onMatchEnded, this, std::placeholders::_1));
+
+}
+
+//Tidy up
+void GameLogger::onUnload() {
+    gameWrapper->UnhookEvent("Function TAGame.GameEvent_Soccar_TA.OnMatchEnded");
+}
+
+//Write the stats to the csv
+void GameLogger::onMatchEnded(std::string eventName) {
+
+    //get the RL ServerWrapper - this gets us access to the stats
+    ServerWrapper sw = gameWrapper->GetOnlineGame();
+    if (sw.IsNull()) {
+        cvarManager->log("ServerWrapper is null. CSV cannot be recorded");
+        return;
+    }
+
+    //create a string stream
+    std::stringstream ss;   
+
+    //get the current time stamp
+    std::string timestamp = getTimestamp("%Y-%m-%d %H:%M:%S");
+
+    //get the game mode e.g. Dual or Doubles
+    std::string gameMode = sw.GetPlaylist().GetTitle().ToString() + ((sw.GetPlaylist().GetbRanked()) ? " (Ranked)" : "");
+    if (gameMode == "") {
+        gameMode = "Unknown";
+    }
+
+    //
+    std::fstream csv;
+
+    //
+    if (!std::filesystem::exists(filename)) {
+        csv.open(filename, std::fstream::out);
+        csv << "Timestamp,Game mode,Team,Player,Score,Goals,Own Goals,Assists,Saves,Shots,Demos,Damage,MVP,Team Score,Win,MMR\n";
+        csv.close();
+    }
+
+    //
+    csv.open(filename, std::fstream::out | std::fstream::app);
+    
+    //update the player stats one last time
+    updatePlayerStats();
+
+    //
+    for (auto const& [playerID, playerStats] : playerStats) {
+        
+        //
+        ss << timestamp << "," << gameMode << "," << playerStats.team << "," << playerStats.name << "," << playerStats.club << "," << playerStats.mmr << "," << playerStats.score << "," << playerStats.goals << "," << playerStats.ownGoals << "," << playerStats.assists << "," << playerStats.saves << "," << playerStats.shots << "," << playerStats.demos << "," << playerStats.damage << "," << playerStats.mvp << "\n";
+        csv << ss.str();
+        
+        //empty the string
+        ss.str(std::string());
+
+    }
+
+    //
+    csv.close();
+
+    //clear the map between rounds
+    playerStats.clear();
+
+}
+
+//Gets the current date/time as a formatted string
+std::string GameLogger::getTimestamp(std::string format) {
+
+    //
+    const std::time_t now = std::time(nullptr);
+    const std::tm currTimeLocal = *std::localtime(&now);
+
+    //
+    std::stringstream ss;
+    ss << std::put_time(&currTimeLocal, format.c_str());
+
+    //
+    return ss.str();
+
+}
+
+//update the player stats map
+void GameLogger::updatePlayerStats() {
+
+    //get the RL ServerWrapper - this gets us access to the stats
+    ServerWrapper sw = gameWrapper->GetOnlineGame();
+    if (sw.IsNull()) {
+        cvarManager->log("ServerWrapper is null. Error getting player stats");
+        return;
+    }
+
+    //get a map of all the stats for each player - should i null check these - prob
+    ArrayWrapper<TeamWrapper> teams = sw.GetTeams();
+    ArrayWrapper<PriWrapper>  players = sw.GetPRIs();
+
+    //
+    MMRWrapper mw = gameWrapper->GetMMRWrapper();
+
+    //
+    for (int i = 0; i < players.Count(); i++) {
+        
+        //
+        PriWrapper player = players.Get(i);
+
+        //
+        std::string playerID = player.GetUniqueIdWrapper().GetIdString();
+
+        //
+        std::string team = ((player.GetTeamNum() == 0) ? "Orange" : "Blue");
+
+        //get the players name
+        std::string name = player.GetPlayerName().ToString();
+
+        //get the club name (formatted as [tag] name)
+        //ClubDetailsWrapper clubDetails = player.GetClubDetails();
+        //std::string club = "[" + clubDetails.GetClubTag().ToString() + "] " + clubDetails.GetClubName().ToString();
+        std::string club = "";
+
+        //calculate the mmr for each player
+        int mmr = 0;
+        int currentPlaylist = mw.GetCurrentPlaylist();
+        if (mw.IsSynced(player.GetUniqueIdWrapper(), currentPlaylist)) {
+            mmr = std::round(mw.GetPlayerMMR(player.GetUniqueIdWrapper(), currentPlaylist));
+        }
+
+        //now for how well they did in the match
+        int score    = player.GetMatchScore();
+        int goals    = player.GetMatchGoals();
+        int ownGoals = player.GetMatchOwnGoals();
+        int assists  = player.GetMatchAssists();
+        int saves    = player.GetMatchSaves();
+        int shots    = player.GetMatchShots();
+        int demos    = player.GetMatchDemolishes();
+        int damage   = player.GetMatchBreakoutDamage();
+        int mvp      = player.GetbMatchMVP();
+
+        //
+        playerStats[playerID] = PlayerAndMatchStats{ team, name, club, mmr, score, goals, ownGoals, assists, saves, shots, demos, damage, mvp };
+
+    }
+
+}
---
+++ GameLogger/GameLogger.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "GuiBase.h"
+#include "bakkesmod/plugin/bakkesmodplugin.h"
+#include "bakkesmod/plugin/pluginwindow.h"
+#include "bakkesmod/plugin/PluginSettingsWindow.h"
+
+#include <map>
+#include <string>
+
+#include "version.h"
+constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
+
+//main player stats
+typedef struct {
+    std::string team;
+    std::string name;
+    std::string club;
+    int mmr;
+    int score;
+    int goals;
+    int ownGoals;
+    int assists;
+    int saves;
+    int shots;
+    int demos;
+    int damage;
+    int mvp;
+} PlayerAndMatchStats;
+
+//
+class GameLogger : public BakkesMod::Plugin::BakkesModPlugin {
+
+public:
+
+    //
+	void onLoad() override;
+	void onUnload() override;
+
+private:
+    
+    //event hooks
+    void onMatchEnded(std::string eventName);
+
+    //
+    std::string getTimestamp(std::string format);
+    void updatePlayerStats();
+ 
+};
